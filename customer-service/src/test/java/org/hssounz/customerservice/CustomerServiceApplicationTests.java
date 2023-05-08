74814a6425b3b396fab45002654a48ad0f3d125f
Hassen <hassenbensaad55@gmail.com>
2023-05-08 11:51:03 +0200

    Initial commit

---
+++ customer-service/src/test/java/org/hssounz/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hssounz.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
